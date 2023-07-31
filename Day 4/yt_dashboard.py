07f40ac0f3a510f2e096885c8aec229eb5fec132
pablo-mv <64136000+pablo-mv@users.noreply.github.com>
2023-07-31 20:36:56 +0200

    Day 4 - Aggregate section Completed the aggregate section of the dashboard. Includes the change over time of certain metrics and a formated visualization of the df

--- Day 4/yt_dashboard.py
+++ Day 4/yt_dashboard.py
@@ -8,9 +8,23 @@ from datetime import datetime
 
 #Define functions
 
-#load data
+def style_negative(v, props=''):
+    """ Style negative values in dataframe"""
+    try: 
+        return props if v < 0 else None
+    except:
+        pass
+    
+def style_positive(v, props=''):
+    """Style positive values in dataframe"""
+    try: 
+        return props if v > 0 else None
+    except:
+        pass    
+
 @st.cache_data
 def load_data():
+    """ Load the data """
     df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
                       'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
@@ -32,8 +46,52 @@ def load_data():
 df_agg, df_agg_sub, df_comments, df_time = load_data()
 
 #Feature engineering
+#Obtain the median aggregate
 df_agg_diff = df_agg.copy()
 metric_date_12mo = df_agg_diff['Video publish time'].max() - pd.DateOffset(months = 12)
+median_agg = df_agg_diff[df_agg_diff['Video publish time'] >= metric_date_12mo].median(numeric_only = True)
+
+#Difference from the median
+numeric_cols = np.array((df_agg_diff.dtypes == 'float64') | (df_agg_diff.dtypes == 'int64'))
+df_agg_diff.iloc[:,numeric_cols] = (df_agg_diff.iloc[:,numeric_cols] - median_agg).div(median_agg)
 
 #Dashboard
-add_sidebar = st.sidebar.selectbox("Aggregate or Individual Video", ("Aggregate Metrics", "Individual Video Analysis"))
+add_sidebar = st.sidebar.selectbox("Aggregate or Individual Video", ('Aggregate Metrics', 'Individual Video Analysis'))
+
+if add_sidebar == 'Aggregate Metrics':
+    st.write("Agg")
+    
+    df_agg_metrics = df_agg[['Video publish time','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
+                             'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
+    metric_date_12mo = df_agg_diff['Video publish time'].max() - pd.DateOffset(months = 12)
+    metric_date_6mo = df_agg_diff['Video publish time'].max() - pd.DateOffset(months = 6)
+    metric_medians6mo = df_agg_metrics[df_agg_metrics['Video publish time'] >= metric_date_6mo].median().drop('Video publish time')
+    metric_medians12mo = df_agg_metrics[df_agg_metrics['Video publish time'] >= metric_date_12mo].median().drop('Video publish time')
+    
+    col1, col2, col3, col4, col5 = st.columns(5)
+    columns = [col1, col2, col3, col4, col5]
+    
+    count = 0
+    for i in metric_medians6mo.index:
+        with columns[count]:
+            delta = (metric_medians6mo[i] - metric_medians12mo[i])/metric_medians12mo[i]
+            st.metric(label= i, value = round(metric_medians6mo[i],1), delta = "{:.2%}".format(delta))
+            count += 1
+            if count >= 5:
+                count = 0
+                
+    #get date information / trim to relevant data 
+    df_agg_diff['Publish_date'] = df_agg_diff['Video publish time'].apply(lambda x: x.date())
+    df_agg_diff_final = df_agg_diff.loc[:,['Video title','Publish_date','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
+                             'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
+    
+    #Figure coloration out
+    df_agg_numeric_lst = df_agg_diff_final.median(numeric_only = True).index.tolist()
+    df_to_pct = {}
+    for i in df_agg_numeric_lst:
+        df_to_pct[i] = '{:.1%}'.format
+    
+    st.dataframe(df_agg_diff_final.style.hide().applymap(style_negative, props='color:red;').applymap(style_positive, props='color:green;').format(df_to_pct))
+    
+if add_sidebar == 'Individual Video Analysis':
+    st.write("Ind")
